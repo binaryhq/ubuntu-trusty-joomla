89c2a07e400c117d363cc1645d34238306a744ff
ningappa <ningappa@users.noreply.github.com>
2016-09-28 14:56:23 +0530

    Update configuration.php

--- uploads/configuration.php
+++ uploads/configuration.php
@@ -56,8 +56,8 @@ class JConfig {
 	public $unicodeslugs = '0';
 	public $feed_limit = '10';
 	public $feed_email = 'none';
-	//public $log_path = 'administrator/logs';
-	//public $tmp_path = 'tmp';
+	public $log_path = 'administrator/logs';
+	public $tmp_path = 'tmp';
 	public $lifetime = '15';
 	public $session_handler = 'database';
 }
